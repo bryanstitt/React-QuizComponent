4d243a3ae936e1aa5aba398b0c0b14831c98a8f1
Bryan Stitt <bryan.stitt1@aa.com>
2021-12-08 16:38:41 -0600

    module 1

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,7 +11,7 @@ class Quiz extends Component {
     render() {
         return (
             <div>
-                <div id="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+                <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
             </div>
         )
     }
